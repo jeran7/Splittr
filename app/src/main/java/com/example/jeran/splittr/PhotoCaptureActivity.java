11ea861ae8c45c98e5d22023e2b0527650c6bd17
jeran7 <jeran7@gmail.com>
2018-03-10 21:23:59 -0800

    Adding photo capture activity

---
+++ app/src/main/java/com/example/jeran/splittr/PhotoCaptureActivity.java
@@ -0,0 +1,142 @@
+package com.example.jeran.splittr;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PhotoCaptureActivity extends AppCompatActivity {
+    private static final String TAG = PhotoCaptureActivity.class.getSimpleName();
+    private TextRecognizer detector;
+    private TextView textView;
+    private String mCurrentPhotoPath;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_capture);
+
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1024 && resultCode == RESULT_OK) {
+            try{
+                startOCR();
+            }catch (Exception e){
+                textView.setText("Problem encoured");
+            }
+        }
+    }
+
+    private void startOCR () {
+        try {
+            Bitmap bitmap = getBitmap();
+            imageView.setImageBitmap(bitmap);
+
+            detector = new TextRecognizer.Builder(this).build();
+            if (detector.isOperational() && null != bitmap) {
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<TextBlock> textBlocks = detector.detect(frame);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < textBlocks.size(); ++ i) {
+                    TextBlock tb = textBlocks.get(i);
+                    if (tb != null && tb.getValue() != null) {
+                        sb.append(tb.getValue());
+                    }
+                }
+                if (textBlocks.size() == 0) {
+                    textView.setText("Scan failed");
+                } else {
+                    textView.setText(sb.toString());
+                }
+            } else {
+                textView.setText("Invalid image");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private Bitmap getBitmap () {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = 5;
+        return BitmapFactory.decodeFile(mCurrentPhotoPath, options);
+    }
+
+    @Override
+    protected void onStop() {
+        if (detector != null) {
+            detector.release();
+        }
+        super.onStop();
+    }
+
+    private void dispatchTakePictureIntent () {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1024);
+            }
+        }
+    }
+
+    private File createImageFile () throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
