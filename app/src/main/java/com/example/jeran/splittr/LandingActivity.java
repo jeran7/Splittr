60f49d680f39fca2c2292437d6c9a3714ce742b6
Dhruv Mevada <mevadadhruv90@gmail.com>
2018-03-16 23:53:28 -0700

    finished integrating the get summary web view

--- app/src/main/java/com/example/jeran/splittr/LandingActivity.java
+++ app/src/main/java/com/example/jeran/splittr/LandingActivity.java
@@ -7,21 +7,31 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.jeran.splittr.helper.JsonCallAsync;
+import com.example.jeran.splittr.helper.LinkUtils;
+import com.example.jeran.splittr.helper.ResponseBin;
+import com.example.jeran.splittr.helper.ResponseListener;
+import com.example.jeran.splittr.helper.ToastUtils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
 /* This is the activity where the users can see his expense. */
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
     private static boolean doubleBackToExitPressedOnce = false;
     FirebaseAuth firebaseAuth;
     DatabaseReference db;
@@ -29,6 +39,9 @@ public class LandingActivity extends AppCompatActivity {
     ArrayList<SummaryListViewDataModel> dataModels;
     ListView listView;
     private static SummaryListViewAdapter adapter;
+
+    private SharedPreferences sharedPreferences;
+    private String email = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,26 +53,26 @@ public class LandingActivity extends AppCompatActivity {
             return;
         }
 
+        FloatingActionButton fb = findViewById(R.id.fab);
         listView = findViewById(R.id.summaryListView);
 
-        dataModels = new ArrayList<>();
-        dataModels.add(new SummaryListViewDataModel("test", 35));
-        dataModels.add(new SummaryListViewDataModel("test2", -35.56));
-        dataModels.add(new SummaryListViewDataModel("test3", 0));
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        email = sharedPreferences.getString(getString(R.string.USER_EMAIL), "");
+        fb.setOnClickListener(this);
 
-        adapter = new SummaryListViewAdapter(dataModels, getApplicationContext());
-        listView.setAdapter(adapter);
+        JSONObject getSummaryObject = new JSONObject();
 
-        FloatingActionButton fb = (FloatingActionButton) findViewById(R.id.fab);
-        fb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        try
+        {
+            getSummaryObject.put("email", email);
+        }
 
-                Intent intent = new Intent(getApplicationContext(), AddBillActivity.class);
-                startActivity(intent);
-            }
-        });
+        catch (JSONException e)
+        {
+            Log.d("Splittr", e.toString());
+        }
 
+        new JsonCallAsync(LandingActivity.this, "getSummaryRequest", getSummaryObject.toString(), LinkUtils.GET_SUMMARY_URL, responseListener, true, "GET").execute();
     }
 
     private boolean isUserLoggedIn() {
@@ -111,4 +124,57 @@ public class LandingActivity extends AppCompatActivity {
         }
 //        }
     }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.fab:
+                Intent intent = new Intent(getApplicationContext(), AddBillActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    ResponseListener responseListener = new ResponseListener() {
+        @Override
+        public void setOnResponseListener(ResponseBin responseBin) {
+            if (responseBin != null && responseBin.getResponse() != null) {
+                String response = responseBin.getResponse();
+
+                try
+                {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String result = jsonObject.getString("result");
+
+                    if(result.equals("success"))
+                    {
+                        JSONArray users = jsonObject.getJSONArray("users");
+                        dataModels = new ArrayList<>();
+
+                        for(int i = 0; i < users.length(); i++)
+                        {
+                            String name = users.getJSONObject(i).getString("name");
+                            double amount = users.getJSONObject(i).getDouble("amount");
+
+                            dataModels.add(new SummaryListViewDataModel(name, amount));
+                        }
+
+                        adapter = new SummaryListViewAdapter(dataModels, getApplicationContext());
+                        listView.setAdapter(adapter);
+                    }
+
+                    else if(result.equals("failed"))
+                    {
+                        ToastUtils.showToast(getApplicationContext(), "Couldn't retrieve summary",false);
+                    }
+                }
+
+                catch(JSONException e)
+                {
+                    Toast.makeText(LandingActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 }
